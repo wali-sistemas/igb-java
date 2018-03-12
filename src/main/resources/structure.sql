9f3a17a6a184a4df783a67f43db3c597eea5bbea
Daniel Botero <dboterov@gmail.com>
2018-03-11 22:30:52 -0500

    Actualizacion esquema sql

--- src/main/resources/structure.sql
+++ src/main/resources/structure.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `igb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `igb`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: igb
@@ -33,7 +35,88 @@ CREATE TABLE `assigned_orders` (
   `customer_name` varchar(100) NOT NULL,
   `company` varchar(10) NOT NULL,
   PRIMARY KEY (`idassigned_orders`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `status` varchar(2) NOT NULL,
+  `whscode` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `company` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `inventory_detail`
+--
+
+DROP TABLE IF EXISTS `inventory_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventory_detail` (
+  `idinventory_detail` int(11) NOT NULL AUTO_INCREMENT,
+  `item` varchar(45) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `idinventory` int(11) NOT NULL,
+  PRIMARY KEY (`idinventory_detail`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `inventory_difference`
+--
+
+DROP TABLE IF EXISTS `inventory_difference`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventory_difference` (
+  `idinventory_difference` int(11) NOT NULL AUTO_INCREMENT,
+  `idinventory` int(11) NOT NULL,
+  `item` varchar(45) NOT NULL,
+  `expected` int(11) NOT NULL,
+  `found` int(11) NOT NULL,
+  PRIMARY KEY (`idinventory_difference`),
+  KEY `id_idx` (`idinventory`),
+  CONSTRAINT `id` FOREIGN KEY (`idinventory`) REFERENCES `inventory` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `packing_list_record`
+--
+
+DROP TABLE IF EXISTS `packing_list_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `packing_list_record` (
+  `idpacking_list_record` int(11) NOT NULL AUTO_INCREMENT,
+  `idpacking_list` int(11) NOT NULL,
+  `order_number` int(11) NOT NULL,
+  `customer_id` varchar(45) NOT NULL,
+  `customer_name` varchar(100) NOT NULL,
+  `datetime_packed` datetime NOT NULL,
+  `picking_order` int(11) NOT NULL,
+  `item_code` varchar(20) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `bin_abs` int(11) NOT NULL,
+  `bin_code` varchar(45) NOT NULL,
+  `box_number` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `employee` varchar(45) NOT NULL,
+  `company_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`idpacking_list_record`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,9 +132,9 @@ CREATE TABLE `packing_order` (
   `status` varchar(10) NOT NULL,
   `customer_id` varchar(45) NOT NULL,
   `customer_name` varchar(100) NOT NULL,
-  `company_name` varchar(45) NOT NULL,
+  `company_name` varchar(45) NOT NULL DEFAULT 'IGB',
   PRIMARY KEY (`idpacking_order`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +151,7 @@ CREATE TABLE `packing_order_item` (
   PRIMARY KEY (`idpacking_order_item`),
   KEY `fk_packing_order_idx` (`idpacking_order`),
   CONSTRAINT `fk_packing_order` FOREIGN KEY (`idpacking_order`) REFERENCES `packing_order` (`idpacking_order`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +171,7 @@ CREATE TABLE `packing_order_item_bin` (
   PRIMARY KEY (`idpacking_order_item_bin`),
   KEY `fk_packing_order_item_idx` (`idpacking_order_item`),
   CONSTRAINT `fk_packing_order_item` FOREIGN KEY (`idpacking_order_item`) REFERENCES `packing_order_item` (`idpacking_order_item`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -112,7 +195,7 @@ CREATE TABLE `picking_record` (
   PRIMARY KEY (`idpicking_record`),
   KEY `idx_picking_emp_id` (`emp_id`),
   KEY `idx_picking_order` (`order_number`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,80 +213,6 @@ CREATE TABLE `users` (
   `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `inventory`
---
-
-DROP TABLE IF EXISTS `inventory`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `inventory` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` datetime NOT NULL,
-  `status` varchar(2) NOT NULL,
-  `storage` varchar(45) NOT NULL,
-  `location` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `inventory_detail`
---
-
-DROP TABLE IF EXISTS `inventory_detail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `inventory_detail` (
-  `idinventory_detail` int(11) NOT NULL AUTO_INCREMENT,
-  `item` varchar(45) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `idinventory` int(11) NOT NULL,
-  PRIMARY KEY (`idinventory_detail`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `inventory_difference`
---
-
-DROP TABLE IF EXISTS `inventory_difference`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `inventory_difference` (
-  `idinventory_difference` int(11) NOT NULL AUTO_INCREMENT,
-  `idinventory` int(11) NOT NULL,
-  `item` varchar(45) NOT NULL,
-  `expected` int(11) NOT NULL,
-  `found` int(11) NOT NULL,
-  PRIMARY KEY (`idinventory_difference`),
-  KEY `id_idx` (`idinventory`),
-  CONSTRAINT `id` FOREIGN KEY (`idinventory`) REFERENCES `inventory` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `packing_list_record`
---
-
-DROP TABLE IF EXISTS `packing_list_record`;
-
-CREATE TABLE `packing_list_record` (
-  `idpacking_list_record` int(11) NOT NULL AUTO_INCREMENT,
-  `idpacking_list` int(11) NOT NULL,
-  `order_number` int(11) NOT NULL,
-  `customer_id` varchar(45) NOT NULL,
-  `customer_name` varchar(100) NOT NULL,
-  `datetime_packed` datetime NOT NULL,
-  `picking_order` int(11) NOT NULL,
-  `item_code` varchar(20) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `bin_abs` int(11) NOT NULL,
-  `bin_code` varchar(45) NOT NULL,
-  `box_number` int(11) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  PRIMARY KEY (`idpacking_list_record`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -215,4 +224,4 @@ CREATE TABLE `packing_list_record` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-17 22:54:32
+-- Dump completed on 2018-03-11 22:29:49
