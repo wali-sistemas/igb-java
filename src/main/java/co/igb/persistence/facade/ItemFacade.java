f5d1e017a30e19e900a337e9b21d10ca6a03433d
jguisaor <43891335+jguisaor@users.noreply.github.com>
2018-11-12 22:38:46 -0500

    consulta-stock (#18)
    * Subiendo ajustes de valor declarado en entrega y comentario en preliminar.
    
    * Subiendo corrección al campo valor declarado por el tipo de dato.
    
    * Subiendo código consulta stock
    
    * Correcciones sugeridas por DanielB

--- src/main/java/co/igb/persistence/facade/ItemFacade.java
+++ src/main/java/co/igb/persistence/facade/ItemFacade.java
@@ -5,6 +5,7 @@ import co.igb.ejb.IGBApplicationBean;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,6 +33,30 @@ public class ItemFacade {
             return (String) persistenceConf.chooseSchema(companyName, DB_TYPE).createNativeQuery(sb.toString()).getSingleResult();
         } catch (Exception e) {
             CONSOLE.log(Level.SEVERE, "Ocurrio un error al consultar el nombre del item " + itemCode, e);
+            return null;
+        }
+    }
+
+    public List<Object[]> getItemStock(String itemCode, String binCode, String whsCode, String companyName) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT CAST(art.ItemCode AS varchar(20)) AS itemCode, CAST(art.ItemName AS varchar(200)) AS itemName, CAST(ubc.OnHandQty AS INT) AS Qty, ");
+        sb.append("       CAST(ubc.WhsCode AS varchar(10)) AS whsCode, CAST(dub.BinCode AS varchar(22)) AS BinCode, CAST(pre.Price AS numeric(18,2)) AS Price ");
+        sb.append("FROM   OITM art ");
+        sb.append("INNER  JOIN OITW sal ON sal.ItemCode = art.ItemCode ");
+        sb.append("INNER  JOIN ITM1 pre ON pre.ItemCode = art.ItemCode ");
+        sb.append("INNER  JOIN OIBQ ubc ON ubc.ItemCode = sal.ItemCode AND ubc.WhsCode = sal.WhsCode ");
+        sb.append("INNER  JOIN OBIN dub ON dub.AbsEntry = ubc.BinAbs ");
+        sb.append("WHERE  sal.OnHand > 0 AND ubc.OnHandQty > 0 AND pre.PriceList = 1 AND (art.ItemCode = '");
+        sb.append(itemCode);
+        sb.append("' OR dub.BinCode = '");
+        sb.append(binCode);
+        sb.append("') AND sal.WhsCode = '");
+        sb.append(whsCode);
+        sb.append("' ORDER BY dub.BinCode");
+        try {
+            return (List<Object[]>) persistenceConf.chooseSchema(companyName, DB_TYPE).createNativeQuery(sb.toString()).getResultList();
+        } catch (Exception e) {
+            CONSOLE.log(Level.SEVERE, "Ocurrio un error al consultar el stock del item " + itemCode + ".", e);
             return null;
         }
     }
