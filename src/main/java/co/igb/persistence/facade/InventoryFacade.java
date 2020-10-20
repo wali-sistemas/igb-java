a63fa67e201de8d0c432057ff8b20b9e0114f4bb
wali <46688722+wali-sistemas@users.noreply.github.com>
2020-10-20 07:45:20 -0500

    ajustes-octubre-2020 (#97)
    * Subiendo item master
    
    * Subiendo cambios
    
    * Subiendo cambios
    
    * Subiendo cambios
    
    * Subiendo cambios
    
    Co-authored-by: wali <sistemas2@igbcolombia.com>

--- src/main/java/co/igb/persistence/facade/InventoryFacade.java
+++ src/main/java/co/igb/persistence/facade/InventoryFacade.java
@@ -1,15 +1,13 @@
 package co.igb.persistence.facade;
 
 import co.igb.persistence.entity.Inventory;
-import co.igb.persistence.entity.Inventory_;
 import co.igb.util.Constants;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -40,18 +38,29 @@ public class InventoryFacade {
     }
 
     public Inventory findLastInventoryOpen(String warehouse, String companyName, boolean testing) {
-        CriteriaBuilder cb = persistenceConf.chooseSchema(companyName, testing, DB_TYPE_WALI).getCriteriaBuilder();
-        CriteriaQuery<Inventory> cq = cb.createQuery(Inventory.class);
-        Root<Inventory> inventory = cq.from(Inventory.class);
-
-        cq.where(cb.equal(inventory.get(Inventory_.status), "PE"),
-                cb.equal(inventory.get(Inventory_.whsCode), warehouse),
-                cb.equal(inventory.get(Inventory_.company), companyName));
-
-        cq.orderBy(cb.desc(inventory.get(Inventory_.id)));
-
+        EntityManager em = persistenceConf.chooseSchema(companyName, testing, DB_TYPE_WALI);
+        StringBuilder sb = new StringBuilder();
+        sb.append("select top 1 cast(id as int)as id,cast(date as datetime)as createDate,cast(status as varchar(3))as status,cast(whscode as varchar(10))as whscode, ");
+        sb.append(" cast(location as varchar(20))as location,cast(company as varchar(15))as company,cast(transfer as int)as transfer ");
+        sb.append("from inventory ");
+        sb.append("where status='PE' and whscode='");
+        sb.append(warehouse);
+        sb.append("' and company='");
+        sb.append(companyName);
+        sb.append("' order by id DESC");
         try {
-            return persistenceConf.chooseSchema(companyName, testing, DB_TYPE_WALI).createQuery(cq).setMaxResults(1).getSingleResult();
+            Object[] obj = (Object[]) em.createNativeQuery(sb.toString()).getSingleResult();
+
+            Inventory entity = new Inventory();
+            entity.setId((Integer) obj[0]);
+            entity.setDate((Date) obj[1]);
+            entity.setStatus((String) obj[2]);
+            entity.setWhsCode((String) obj[3]);
+            entity.setLocation((String) obj[4]);
+            entity.setCompany((String) obj[5]);
+            entity.setTransfer((Integer) obj[6]);
+
+            return entity;
         } catch (NoResultException e) {
             return null;
         } catch (Exception e) {
