ff70c0f19afc654a71678e0f86eefaddc45705a2
wali <46688722+wali-sistemas@users.noreply.github.com>
2021-04-13 09:24:54 -0500

    2021-febrero-jguisao (#115)
    * Subiendo service layer.
    
    * Subiendo cambios en entity
    
    * Subiendo cambios en facade
    
    * Subiendo cambios en rest
    
    * Subiendo cambios en rest
    
    * Subiendo cambios en ejb
    
    * Subiendo cambios en facade y rest
    
    * Subiendo cambios en facade
    
    * Subiendo cambios en JDBC
    
    Co-authored-by: wali <sistemas2@igbcolombia.com>

--- src/main/java/co/igb/persistence/facade/PersistenceConf.java
+++ src/main/java/co/igb/persistence/facade/PersistenceConf.java
@@ -11,43 +11,26 @@ import java.util.logging.Logger;
 public class PersistenceConf {
     private static final Logger CONSOLE = Logger.getLogger(BinLocationFacade.class.getSimpleName());
 
-    @PersistenceContext(unitName = "IGBPU")
-    private EntityManager emIGB;
-    @PersistenceContext(unitName = "IGBPruebasPU")
-    private EntityManager emIGBPruebas;
-    @PersistenceContext(unitName = "VARROCPU")
-    private EntityManager emVARROC;
-    @PersistenceContext(unitName = "VARROCPruebasPU")
-    private EntityManager emVARROCPruebas;
-    @PersistenceContext(unitName = "VELEZPU")
-    private EntityManager emVELEZ;
     @PersistenceContext(unitName = "WMSPU")
     private EntityManager emWMS;
     @PersistenceContext(unitName = "HANAIGBPU")
     private EntityManager emHANAIGB;
     @PersistenceContext(unitName = "HANAVARROCPU")
     private EntityManager emHANAVARROC;
+    @PersistenceContext(unitName = "HANAVELEZPU")
+    private EntityManager emHANAVELEZ;
 
     public EntityManager chooseSchema(String companyName, boolean testing, String dbType) {
-
         if (dbType.equalsIgnoreCase(Constants.DATABASE_TYPE_WALI)) {
             return emWMS;
         } else {
             switch (companyName) {
                 case "IGB":
-                    return emIGB;
+                    return emHANAIGB;
                 case "VARROC":
-                    return emVARROC;
-                case "IGBPruebas":
-                    return emHANAIGB;
-                case "DBIGBTH":
-                    return emHANAIGB;
-                case "VARROCPruebas":
-                    return emHANAVARROC;
-                case "DBVARROCTH":
                     return emHANAVARROC;
                 case "VELEZ":
-                    return emVELEZ;
+                    return emHANAVELEZ;
                 default:
                     return null;
             }
