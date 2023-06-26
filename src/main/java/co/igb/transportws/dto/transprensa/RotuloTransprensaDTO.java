72c58da2265623f4a6400c241b168a0a2ba9e47e
wali <46688722+wali-sistemas@users.noreply.github.com>
2023-06-26 10:12:20 -0500

    master (#195)
    * Subir cambios
    
    * Subir cambios 2019
    
    * Subir cambios
    
    * Subir cambios a redplas
    
    * Subir cambios a redplas dashboard
    
    * Subir cambios a master
    
    * Subir cambios a transportadora
    
    * Subir cambios al master
    
    * Subir cambios al master
    
    * Subir cambios al PackingREST
    
    * Subir cambios al Dashboard
    
    * Subir cambios al Dashboard
    
    * Subir cambios al Dashboard
    
    * Subir cambios
    
    * Subir cambios a master
    
    * Subir cambios a master
    
    * Subir cambios a master

--- src/main/java/co/igb/transportws/dto/transprensa/RotuloTransprensaDTO.java
+++ src/main/java/co/igb/transportws/dto/transprensa/RotuloTransprensaDTO.java
@@ -1,5 +1,6 @@
 package co.igb.transportws.dto.transprensa;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
@@ -7,6 +8,7 @@ import java.io.Serializable;
 /**
  * @author jguisao
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RotuloTransprensaDTO implements Serializable {
     @JsonProperty("guia_inicial")
     protected String guiaInicial;
@@ -27,5 +29,13 @@ public class RotuloTransprensaDTO implements Serializable {
 
     public void setGuiaFinal(String guiaFinal) {
         this.guiaFinal = guiaFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "RotuloTransprensaDTO{" +
+                "guia_inicial='" + guiaInicial + '\'' +
+                ", guia_final='" + guiaFinal + '\'' +
+                '}';
     }
 }
